cc43df476b8e370f105c5ecb3b6519d0b591e329
Dominic Bär <dominic.baer@students.fhnw.ch>
2017-01-16 09:39:49 +0100

    add Testing Build

---
+++ projects/NavVRTesting/Intermediate/Build/Win64/VRExpPluginExample/Shipping/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
@@ -0,0 +1,1 @@
+#include "D:\navVR\projects\NavVRReal\Intermediate\Build\Win64\UE4\Shipping\UELinkerFixupsName.h"
---
+++ projects/NavVRTesting/Intermediate/Build/Win64/VRExpPluginExample/Shipping/UELinkerFixups/UELinkerFixupsName.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UELINKERFIXUPSNAME_H__
+#define __AUTO_UELINKERFIXUPSNAME_H__
+//Last Write: 11.01.2017 12:35:42 +00:00
+#include "D:\navVR\projects\NavVRReal\Intermediate\Build\Win64\UE4\Shipping\UELinkerFixupsName.h"
+#endif//__AUTO_UELINKERFIXUPSNAME_H__
